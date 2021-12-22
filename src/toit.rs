6a05c64d3b8a24e6b90c7abc52722284ec9eba43
Juraj Michálek <juraj.michalek@espressif.com>
2021-12-22 14:31:36 +0100

    tools: add toit jaguar installer (#37)

---
+++ src/toit.rs
@@ -0,0 +1,176 @@
+use std::env;
+use clap::Arg;
+use clap_nested::{Command, Commander, MultiCommand};
+
+use dirs::home_dir;
+use std::path::Path;
+use std::fs::{create_dir_all, remove_dir_all};
+use std::io::Read;
+use std::process::Stdio;
+use crate::config::get_tool_path;
+use crate::package::{prepare_package, prepare_package_strip_prefix, prepare_single_binary};
+use crate::shell::run_command;
+
+struct ToitTools {
+    jaguar_dist_file: String,
+    jaguar_dist_url: String,
+    jaguar_destination_dir: String
+}
+
+
+fn build_toit_tools() -> ToitTools {
+    let jaguar_dist_file = format!("jag_windows.zip");
+    let jaguar_dist_url = format!("https://github.com/toitlang/jaguar/releases/latest/download/{}", jaguar_dist_file);
+
+    ToitTools {
+        jaguar_dist_file,
+        jaguar_dist_url,
+        jaguar_destination_dir: format!("{}/AppData/Local/Programs/jaguar", home_dir().unwrap().display().to_string()),
+    }
+}
+
+#[cfg(windows)]
+fn set_env_variable(key:&str, value:String) {
+    use winreg::{enums::HKEY_CURRENT_USER, RegKey};
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+    let (env, _) = hkcu.create_subkey("Environment").unwrap(); // create_subkey opens with write permissions
+    env.set_value(key, &value).unwrap();
+}
+
+#[cfg(windows)]
+fn update_env_path(value: &str) {
+    let path_key = "PATH";
+    use winreg::{enums::HKEY_CURRENT_USER, RegKey};
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+    let env = hkcu.open_subkey("Environment").unwrap();
+    let env_path:String = env.get_value(path_key).unwrap();
+    if !env_path.contains(&value) {
+        let updated_env_path = format!("{};{}", env_path, value);
+        set_env_variable(path_key, updated_env_path);
+    }
+}
+
+#[cfg(unix)]
+fn update_env_path(value: &str) {
+}
+
+#[cfg(unix)]
+fn set_env_variable(key:&str, value:&str) {
+
+}
+
+fn install_toit_tools(toit_tools:&ToitTools) {
+
+    if Path::new(&toit_tools.jaguar_destination_dir.as_str()).exists() {
+        println!("Previous installation of Toit - Jaguar exist in: {}", toit_tools.jaguar_destination_dir);
+        println!("Please, remove the directory before new installation.");
+    } else {
+        prepare_package(toit_tools.jaguar_dist_url.to_string(),
+                                     &toit_tools.jaguar_dist_file,
+                                     toit_tools.jaguar_destination_dir.to_string());
+    }
+
+    #[cfg(windows)]
+    println!("PATH+=\";{}\"", &toit_tools.jaguar_destination_dir);
+    #[cfg(unix)]
+    println!("export PATH=\"{}:$PATH\"", &toit_tools.jaguar_destination_dir);
+
+    update_env_path(&toit_tools.jaguar_destination_dir);
+
+}
+
+fn uninstall_toit_tools(toit_tools:&ToitTools) {
+    if Path::new(toit_tools.jaguar_destination_dir.as_str()).exists() {
+        println!("Removing: {}", toit_tools.jaguar_destination_dir);
+        remove_dir_all(&toit_tools.jaguar_destination_dir);
+    }
+}
+
+fn get_default_toit_tools(matches: &clap::ArgMatches<'_>) -> ToitTools {
+    build_toit_tools()
+}
+
+fn get_install_runner(_args: &str, matches: &clap::ArgMatches<'_>) -> std::result::Result<(), clap::Error> {
+    let toit_tools = get_default_toit_tools(&matches);
+    if matches.is_present("jaguar") {
+        install_toit_tools(&toit_tools);
+    }
+    Ok(())
+}
+
+fn get_reinstall_runner(_args: &str, matches: &clap::ArgMatches<'_>) -> std::result::Result<(), clap::Error> {
+    let toit_tools = get_default_toit_tools(matches);
+    if matches.is_present("jaguar") {
+        uninstall_toit_tools(&toit_tools);
+        install_toit_tools(&toit_tools);
+    }
+    Ok(())
+}
+
+fn get_uninstall_runner(_args: &str, matches: &clap::ArgMatches<'_>) -> std::result::Result<(), clap::Error> {
+    let toit_tools = get_default_toit_tools(matches);
+    if matches.is_present("jaguar") {
+        uninstall_toit_tools(&toit_tools);
+    }
+    Ok(())
+}
+
+pub fn get_install_cmd<'a>() -> Command<'a, str> {
+    Command::new("install")
+        .description("Install Toit environment")
+        .options(|app| {
+            app.arg(
+                Arg::with_name("jaguar")
+                    .short("j")
+                    .long("jaguar")
+
+            )
+        })
+        .runner(|_args, matches|
+            get_install_runner(_args, matches)
+        )
+}
+
+pub fn get_reinstall_cmd<'a>() -> Command<'a, str> {
+    Command::new("reinstall")
+        .description("Re-install Toit environment")
+        .options(|app| {
+            app.arg(
+                Arg::with_name("jaguar")
+                    .short("j")
+                    .long("jaguar")
+            )
+        })
+        .runner(|_args, matches|
+            get_reinstall_runner(_args, matches)
+        )
+}
+
+pub fn get_uninstall_cmd<'a>() -> Command<'a, str> {
+    Command::new("uninstall")
+        .description("Uninstall Toit environment")
+        .options(|app| {
+            app.arg(
+                Arg::with_name("jaguar")
+                    .short("j")
+                    .long("jaguar")
+            )
+
+        })
+        .runner(|_args, matches|
+            get_uninstall_runner(_args, matches)
+        )
+}
+
+pub fn get_multi_cmd<'a>() -> MultiCommand<'a, str, str> {
+    let multi_cmd: MultiCommand<str, str> = Commander::new()
+        .add_cmd(get_install_cmd())
+        .add_cmd(get_reinstall_cmd())
+        .add_cmd(get_uninstall_cmd())
+        .into_cmd("toit")
+
+        // Optionally specify a description
+        .description("Toit environment.");
+
+    return multi_cmd;
+}
