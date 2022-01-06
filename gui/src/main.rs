05e473b3c0e197c38226f252e1582c13f50045a4
Juraj Michálek <juraj.michalek@espressif.com>
2022-01-06 13:23:59 +0100

    use GitHub pages to host installer web content

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -8,7 +8,8 @@ use crate::gui::webview::open_url;
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 async fn app() -> Result<()> {
-    open_url("http://localhost:8000");
+    // open_url("http://localhost:8000");
+    open_url("https://espressif.github.io/idf-env/gui/assets/#/modify");
     Ok(())
 }
 
